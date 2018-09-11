a998747001aa44cbb85e5cca02c58e2395fcb91d
Zuriel Diaz <zuriel.diaz@ibm.com>
2018-09-11 17:53:02 -0500

    init.

---
+++ src/main/java/com/zlab/msdev/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zlab.msdev.springbootmicroserviceforexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+}
